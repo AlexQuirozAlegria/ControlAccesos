8788e127e471cda1f7111e708df30217effda745
AlexQuirozAlegria <quirozc128@gmail.com>
2025-07-06 17:49:30 -0600

    loginComponenteYFORM

---
+++ ControlAccesos.DesktopUI/FrmLogin.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace ControlAccesos.DesktopUI
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            loginComponente1 = new loginComponente();
+            SuspendLayout();
+            // 
+            // loginComponente1
+            // 
+            loginComponente1.Location = new Point(22, 15);
+            loginComponente1.Name = "loginComponente1";
+            loginComponente1.Size = new Size(534, 322);
+            loginComponente1.TabIndex = 0;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(481, 320);
+            Controls.Add(loginComponente1);
+            Name = "FrmLogin";
+            Text = "FrmLogin";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private loginComponente loginComponente1;
+    }
+}
---
+++ ControlAccesos.DesktopUI/loginComponente.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ControlAccesos.DesktopUI
+{
+    partial class loginComponente
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre de Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(86, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(178, 82);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(217, 27);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(178, 137);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(217, 27);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(301, 201);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Ingresar";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(178, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 28);
+            label3.TabIndex = 5;
+            label3.Text = "Login";
+            // 
+            // loginComponente
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label3);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "loginComponente";
+            Size = new Size(427, 258);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label label3;
+    }
+}
