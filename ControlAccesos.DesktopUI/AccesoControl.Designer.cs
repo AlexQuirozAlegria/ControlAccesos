bf343f07cf2584e26d9232e194f3f2e5ea307217
AlexQuirozAlegria <quirozc128@gmail.com>
2025-07-06 18:25:05 -0600

    ddddd

--- ControlAccesos.DesktopUI/AccesoControl.Designer.cs
+++ ControlAccesos.DesktopUI/AccesoControl.Designer.cs
@@ -45,9 +45,11 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(lblResultado);
-            groupBox1.Location = new Point(242, 61);
+            groupBox1.Location = new Point(284, 77);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(440, 268);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(503, 315);
             groupBox1.TabIndex = 9;
             groupBox1.TabStop = false;
             groupBox1.Text = "Información del Acceso";
@@ -56,9 +58,9 @@
             // 
             lblResultado.AutoSize = true;
             lblResultado.Font = new Font("Segoe UI", 12F);
-            lblResultado.Location = new Point(24, 31);
+            lblResultado.Location = new Point(27, 36);
             lblResultado.Name = "lblResultado";
-            lblResultado.Size = new Size(169, 25);
+            lblResultado.Size = new Size(183, 28);
             lblResultado.TabIndex = 0;
             lblResultado.Text = "Esperando código...";
             lblResultado.Click += lblResultado_Click;
@@ -66,18 +68,19 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(242, 8);
+            label1.Location = new Point(277, 9);
             label1.Name = "label1";
-            label1.Size = new Size(196, 17);
+            label1.Size = new Size(217, 20);
             label1.TabIndex = 5;
             label1.Text = "Escanear o ingresar Código QR:";
             // 
             // btnSalida
             // 
             btnSalida.AutoSize = true;
-            btnSalida.Location = new Point(106, 283);
+            btnSalida.Location = new Point(128, 338);
+            btnSalida.Margin = new Padding(3, 4, 3, 4);
             btnSalida.Name = "btnSalida";
-            btnSalida.Size = new Size(130, 43);
+            btnSalida.Size = new Size(149, 51);
             btnSalida.TabIndex = 8;
             btnSalida.Text = "Registrar Salida";
             btnSalida.UseVisualStyleBackColor = true;
@@ -85,25 +88,28 @@
             // btnEntrada
             // 
             btnEntrada.AutoSize = true;
-            btnEntrada.Location = new Point(106, 234);
+            btnEntrada.Location = new Point(128, 280);
+            btnEntrada.Margin = new Padding(3, 4, 3, 4);
             btnEntrada.Name = "btnEntrada";
-            btnEntrada.Size = new Size(130, 43);
+            btnEntrada.Size = new Size(149, 51);
             btnEntrada.TabIndex = 7;
             btnEntrada.Text = "Registrar Entrada";
             btnEntrada.UseVisualStyleBackColor = true;
             // 
             // txtCodigoQR
             // 
-            txtCodigoQR.Location = new Point(444, 30);
+            txtCodigoQR.Location = new Point(514, 40);
+            txtCodigoQR.Margin = new Padding(3, 4, 3, 4);
             txtCodigoQR.Name = "txtCodigoQR";
-            txtCodigoQR.Size = new Size(217, 25);
+            txtCodigoQR.Size = new Size(247, 27);
             txtCodigoQR.TabIndex = 6;
             // 
             // btnValidacion
             // 
-            btnValidacion.Location = new Point(8, 283);
+            btnValidacion.Location = new Point(16, 338);
+            btnValidacion.Margin = new Padding(3, 4, 3, 4);
             btnValidacion.Name = "btnValidacion";
-            btnValidacion.Size = new Size(91, 39);
+            btnValidacion.Size = new Size(104, 51);
             btnValidacion.TabIndex = 10;
             btnValidacion.Text = "Validar QR";
             btnValidacion.UseVisualStyleBackColor = true;
@@ -111,9 +117,10 @@
             // pictureBoxCamera
             // 
             pictureBoxCamera.BackColor = SystemColors.ActiveCaptionText;
-            pictureBoxCamera.Location = new Point(3, 3);
+            pictureBoxCamera.Location = new Point(10, 9);
+            pictureBoxCamera.Margin = new Padding(3, 4, 3, 4);
             pictureBoxCamera.Name = "pictureBoxCamera";
-            pictureBoxCamera.Size = new Size(228, 212);
+            pictureBoxCamera.Size = new Size(261, 249);
             pictureBoxCamera.TabIndex = 11;
             pictureBoxCamera.TabStop = false;
             // 
@@ -121,24 +128,26 @@
             // 
             comboBoxCameras.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxCameras.FormattingEnabled = true;
-            comboBoxCameras.Location = new Point(245, 30);
+            comboBoxCameras.Location = new Point(287, 40);
+            comboBoxCameras.Margin = new Padding(3, 4, 3, 4);
             comboBoxCameras.Name = "comboBoxCameras";
-            comboBoxCameras.Size = new Size(193, 25);
+            comboBoxCameras.Size = new Size(220, 28);
             comboBoxCameras.TabIndex = 12;
             // 
             // btnEscanear
             // 
-            btnEscanear.Location = new Point(12, 238);
+            btnEscanear.Location = new Point(16, 280);
+            btnEscanear.Margin = new Padding(3, 4, 3, 4);
             btnEscanear.Name = "btnEscanear";
-            btnEscanear.Size = new Size(87, 39);
+            btnEscanear.Size = new Size(104, 51);
             btnEscanear.TabIndex = 13;
-            btnEscanear.Text = "Escanear";
+            btnEscanear.Text = "Iniciar";
             btnEscanear.UseVisualStyleBackColor = true;
             btnEscanear.Click += btnEscanear_Click;
             // 
             // AccesoControl
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(btnEscanear);
             Controls.Add(label1);
@@ -149,8 +158,9 @@
             Controls.Add(btnSalida);
             Controls.Add(btnEntrada);
             Controls.Add(txtCodigoQR);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AccesoControl";
-            Size = new Size(689, 344);
+            Size = new Size(813, 405);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxCamera).EndInit();
