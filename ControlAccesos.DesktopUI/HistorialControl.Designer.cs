fba7600f477e4c27f09629613872f2eac27b32b1
AlexQuirozAlegria <quirozc128@gmail.com>
2025-07-06 21:45:31 -0600

    omg ya filtros

--- ControlAccesos.DesktopUI/HistorialControl.Designer.cs
+++ ControlAccesos.DesktopUI/HistorialControl.Designer.cs
@@ -34,14 +34,24 @@
             label4 = new Label();
             dtpFechaInicio = new DateTimePicker();
             label3 = new Label();
+            txtInvitadoID = new TextBox();
+            txtResidenteID = new TextBox();
+            cmbTipo = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            txtGuardiaID = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            txtPlacas = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dgvHistorial).BeginInit();
             SuspendLayout();
             // 
             // btnFiltrarHistorial
             // 
-            btnFiltrarHistorial.Location = new Point(639, 43);
+            btnFiltrarHistorial.Location = new Point(647, 347);
             btnFiltrarHistorial.Name = "btnFiltrarHistorial";
-            btnFiltrarHistorial.Size = new Size(94, 29);
+            btnFiltrarHistorial.Size = new Size(86, 29);
             btnFiltrarHistorial.TabIndex = 11;
             btnFiltrarHistorial.Text = "Filtrar";
             btnFiltrarHistorial.UseVisualStyleBackColor = true;
@@ -49,10 +59,10 @@
             // dgvHistorial
             // 
             dgvHistorial.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvHistorial.Location = new Point(18, 78);
+            dgvHistorial.Location = new Point(18, 82);
             dgvHistorial.Name = "dgvHistorial";
             dgvHistorial.RowHeadersWidth = 51;
-            dgvHistorial.Size = new Size(715, 288);
+            dgvHistorial.Size = new Size(715, 259);
             dgvHistorial.TabIndex = 10;
             // 
             // dtpFechaFin
@@ -87,10 +97,102 @@
             label3.TabIndex = 6;
             label3.Text = "Desde:";
             // 
+            // txtInvitadoID
+            // 
+            txtInvitadoID.Location = new Point(260, 48);
+            txtInvitadoID.Name = "txtInvitadoID";
+            txtInvitadoID.Size = new Size(58, 27);
+            txtInvitadoID.TabIndex = 12;
+            // 
+            // txtResidenteID
+            // 
+            txtResidenteID.Location = new Point(105, 48);
+            txtResidenteID.Name = "txtResidenteID";
+            txtResidenteID.Size = new Size(58, 27);
+            txtResidenteID.TabIndex = 13;
+            // 
+            // cmbTipo
+            // 
+            cmbTipo.FormattingEnabled = true;
+            cmbTipo.Items.AddRange(new object[] { "Entrada", "Salida" });
+            cmbTipo.Location = new Point(647, 48);
+            cmbTipo.Name = "cmbTipo";
+            cmbTipo.Size = new Size(86, 28);
+            cmbTipo.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(600, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 20);
+            label1.TabIndex = 15;
+            label1.Text = "Tipo:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 20);
+            label2.TabIndex = 16;
+            label2.Text = "Residente ID:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(169, 51);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 20);
+            label5.TabIndex = 17;
+            label5.Text = "Invitado ID:";
+            // 
+            // txtGuardiaID
+            // 
+            txtGuardiaID.Location = new Point(413, 48);
+            txtGuardiaID.Name = "txtGuardiaID";
+            txtGuardiaID.Size = new Size(58, 27);
+            txtGuardiaID.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(324, 51);
+            label6.Name = "label6";
+            label6.Size = new Size(83, 20);
+            label6.TabIndex = 19;
+            label6.Text = "Guardia ID:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(477, 51);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 20);
+            label7.TabIndex = 20;
+            label7.Text = "Placas:";
+            // 
+            // txtPlacas
+            // 
+            txtPlacas.Location = new Point(536, 48);
+            txtPlacas.Name = "txtPlacas";
+            txtPlacas.Size = new Size(58, 27);
+            txtPlacas.TabIndex = 21;
+            // 
             // HistorialControl
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(txtPlacas);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(txtGuardiaID);
+            Controls.Add(label5);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cmbTipo);
+            Controls.Add(txtResidenteID);
+            Controls.Add(txtInvitadoID);
             Controls.Add(btnFiltrarHistorial);
             Controls.Add(dgvHistorial);
             Controls.Add(dtpFechaFin);
@@ -112,5 +214,15 @@
         private Label label4;
         private DateTimePicker dtpFechaInicio;
         private Label label3;
+        private TextBox txtInvitadoID;
+        private TextBox txtResidenteID;
+        private ComboBox cmbTipo;
+        private Label label1;
+        private Label label2;
+        private Label label5;
+        private TextBox txtGuardiaID;
+        private Label label6;
+        private Label label7;
+        private TextBox txtPlacas;
     }
 }
